6a49ef3804351e76ffa638b41c69d3552d83b2e0
Andre Gomes <andgomes95@gmail.com>
2017-04-18 01:06:25 -0300

    Adicionado informações no READ ME e inserção de um código exemplo para teste

---
+++ teste.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+Analisador Lexico
+Arquivo de teste
+*/
+
+int main() {
+  int d=0;
+  float a=7.77;
+  for(d=0;d<10;d++){
+    printf("%d\n",d);
+  }
+  §
+  22dogs = 25;
+  int __ = 5;
+  return 0;
+}
